152030ebdf2d3b4ee13dd196fee53433a472c1a4
rosalyncho <18chor67@millshighsch.org>
2016-08-29 09:43:16 -0700

    Monday 8/29/16 Classwork

---
+++ HelloWorld/src/Pikachu.java
@@ -0,0 +1,9 @@
+
+public class Pikachu {
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t  (\\__/)");
+		System.out.println("\t  (o^.^)");
+		System.out.println("\tz(_(\")(\")");
+	}
+}
