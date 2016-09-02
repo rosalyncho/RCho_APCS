4f105471321f38ac6532e9bfdfbbd93b603bc600
rosalyncho <18chor67@millshighsch.org>
2016-09-02 09:48:04 -0700

    Friday 9/2/2016 Classwork

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,4 +4,19 @@ public class Calculate {
 		int x=num*num;
 		return x;
 	}
-}
+	public static int cube (int number) {
+		int answer;
+		answer = number*number*number;
+		return (answer);
+	}
+	public static double average (double number, double num) {
+		return number+num/2;
+	}
+	public static double average (double number, double num, double n) {
+		return number+num+n/3;
+	}
+	public static double toDegrees (double number) {
+		return number
+	}
+	
+}	
