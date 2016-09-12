9b2e51ec4159036903e204affd4e4f1bb07480a7
rosalyncho <18chor67@millshighsch.org>
2016-09-12 09:49:49 -0700

    Monday 9/12/2016 Classwork

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,19 +4,46 @@ public class Calculate {
 		int x=num*num;
 		return x;
 	}
+	
 	public static int cube (int number) {
 		int answer;
 		answer = number*number*number;
 		return (answer);
 	}
+	
 	public static double average (double number, double num) {
 		return number+num/2;
 	}
+	
 	public static double average (double number, double num, double n) {
 		return number+num+n/3;
 	}
-	public static double toDegrees (double number) {
-		return number
+	
+	public static double toDegrees (double radians) {
+		double answer;
+		answer = 180/3.14159;
+		return (answer);			
+	}
+	
+	public static double toRadians (double degrees) {
+		double answer;
+		answer = 3.14159/180;
+		return (answer);
+	}
+	
+	public static double discriminant (double a, double b, double c) {
+		double answer;
+		answer = b*b - 4*a*c;
+		return answer;
+	}
+	
+	public static String toImproperFrac (int a, int b, int c) {
+		int answer;
+		answer = 
+	}
+	
+
+		
 	}
 	
 }	
