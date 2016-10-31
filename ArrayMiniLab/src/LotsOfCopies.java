9e7706d9281cf5fc13b73da3f2e46252c3cc97b3
rosalyncho <18chor67@millshighsch.org>
2016-10-31 09:50:05 -0700

    lakjsfdl

--- ArrayMiniLab/src/LotsOfCopies.java
+++ ArrayMiniLab/src/LotsOfCopies.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
 
 public class LotsOfCopies {
 	public static void main (String[] args) {
 		int num = 7;
 		String strMain = "APCS";
 		int[] arrMain = {1, 2, 3, 4, 5};
+		System.out.println(num);
+		System.out.println(strMain);
+		System.out.println(Arrays.toString(arrMain));
 	}
 	public static void changMe (int x, String str, int[] arr) {
-		x = 5;
-		str = (x*x + "/" + x);
-		arr = {2, 4, 6, 8, 10};
+		
 	}
 }
