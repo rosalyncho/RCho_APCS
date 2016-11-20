fc73e302a6ffbacbb8901a61edf7ceb3066e5f7e
rosalyncho <18chor67@millshighsch.org>
2016-11-19 21:39:36 -0800

    frac calc

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -11,7 +11,7 @@ public class FracCalc {
         // TODO: Read the input from the user and call produceAnswer with an equation
     	Scanner console = new Scanner(System.in);
     	System.out.println("Enter your operation.");
-    	String input = console.next();
+    	String input = console.nextLine();
     	if (input.equals("quit")){
     		String answer=produceAnswer(input);
     		System.out.println(answer);
@@ -30,19 +30,32 @@ public class FracCalc {
     public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
+    	String firstOperand = new String();
+    	String operator = new String();
+    	String secondOperand = new String();
     	String[] splitted = input.split(" "); //parses by spaces
-    	
-  //  	int firstSpace = input.indexOf(" ");
-  //  	int secondSpace = input.indexOf(" ", firstSpace + 2);
-  //  	if(firstSpace < 0){
-  //  		System.out.println("Not an operation");
-  //  	}
+    	int lengthOfInput = input.length(); 
+  //    System.out.println(Arrays.toString(splitted));
+    	int firstSpace = input.indexOf(" ");
+    	int secondSpace = input.indexOf(" ", firstSpace + 2);
+    	operator = input.substring(firstSpace+1, secondSpace);
   //  	String firstOperand = input.substring(firstSpace, secondSpace);
-    	//String operator
+  //  	System.out.println(operator);
+    	firstOperand = input.substring(0, firstSpace);
+  //  	System.out.println(firstOperand);
+    	secondOperand = input.substring(secondSpace+1, lengthOfInput);
+   // 	System.out.println(secondOperand);
     	
-        return "";
+        return parseFractions(secondOperand);
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
-    
+    public static String parseFractions(String secondOperand)
+    {
+  //  	if (secondOperand.indexOf("_")==-1 && secondOperand.indexOf("/")==-1) {
+    		String wholeNumber = secondOperand.substring(0, secondOperand.indexOf("_"));
+    		String numerator = secondOperand.substring(secondOperand.indexOf("_") + 1, secondOperand.indexOf("/")); //gets the value starting from position 0 to the "_"
+    		String denominator = secondOperand.substring(secondOperand.indexOf("/") + 1, secondOperand.length());
+    		return ("whole:" + wholeNumber + " numerator:" + numerator + " denominator:" + denominator);
+    }
 }
